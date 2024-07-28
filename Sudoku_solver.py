dfa537ff6c905b67073cd96e4bc761f83861c38b
shipra <160950734+shipraa1@users.noreply.github.com>
2024-07-29 02:16:07 +0530

    first commit +1

--- Sudoku_solver.py
+++
@@ -1,2 +0,0 @@
-# Sudoku_solver
-A Sudoku solver is a program or algorithm designed to solve Sudoku puzzles, which are logic-based number placement puzzles. The solver typically uses techniques such as backtracking, constraint propagation, and various heuristic methods
